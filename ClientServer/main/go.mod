acda63eabf7e99a96fd04ae0bb7399356901091a
Tan-CM <102352034+Tan-CM@users.noreply.github.com>
2022-06-22 17:27:45 +0800

    Added main and config folders

---
+++ ClientServer/main/go.mod
@@ -0,0 +1,12 @@
+module ProjectGoLive/ClientServer/main
+
+go 1.18
+
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/joho/godotenv v1.4.0
+	github.com/satori/go.uuid v1.2.0
+	golang.org/x/crypto v0.0.0-20220525230936-793ad666bf5e
+)
+
+require gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
---
+++ ClientServer/main/user.go
@@ -0,0 +1,18 @@
+package main
+
+// Initialise the bussiness logic or any handshake with back end server
+func userInit() {
+
+}
+
+// Request for new session ID for new session
+func userRequestSessionID() {
+
+}
+
+// get the user username, password and user type
+func userGetUser() {
+
+}
+
+// returns boolean true of false if the user is a valid
