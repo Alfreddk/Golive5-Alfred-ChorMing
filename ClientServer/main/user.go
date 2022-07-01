eb6a892913af9f7b189e0f118083b6939a8e6a27
Tan-CM <tanchorming@gmail.com>
2022-07-01 13:10:14 +0800

    Added Mutex to sync local and remote DB for User and Items

--- ClientServer/main/user.go
+++ ClientServer/main/user.go
@@ -2,12 +2,18 @@ package main
 
 import (
 	"log"
+	"sync"
 )
 
+var mutex2 sync.Mutex
 var mapUsers = map[string]User{}
 
 // userInit performs getAllUsers() function call to retreive all users records and maps these records to mapUsers on runtime memory.
 func userInit() {
+
+	mutex2.Lock()
+	defer mutex2.Unlock()
+
 	users, err := getAllUsers()
 	if err != nil {
 		log.Fatalln(err)
